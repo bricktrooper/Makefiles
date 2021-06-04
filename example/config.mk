cbad2289ee594e3e90b708a53ed7f10b611d7963
bricktrooper <kylepinto@yahoo.ca>
2021-06-03 23:11:27 -0400

    Add demo program to illustrate Makefile usage

---
+++ example/config.mk
@@ -0,0 +1,45 @@
+# ======= BUILD CONFIG ======= #
+
+# --- COMPILER --- #
+
+CC = gcc
+
+# --- LINKER --- #
+
+LD = gcc
+
+# --- DEPENDENCY GENERATOR --- #
+
+MM = gcc
+
+# --- COMPILER FLAGS --- #
+
+CCFLAGS += -std=c17
+CCFLAGS += -Wall
+CCFLAGS += -Wpedantic
+
+# --- LINKER FLAGS --- #
+
+LDFLAGS += -std=c17
+LDFLAGS += -Wall
+LDFLAGS += -Wpedantic
+
+# --- LINKER LIBRARIES --- #
+
+LDLIBS +=
+
+# --- ARTIFACTS FOLDER --- #
+
+ART = bin
+
+# --- BINARY EXECUTABLE NAME --- #
+
+EXE = demo
+
+# --- SOURCE FILE DIRECTORIES --- #
+
+DIR += hello
+DIR += test
+DIR += hey
+
+# ============================ #
